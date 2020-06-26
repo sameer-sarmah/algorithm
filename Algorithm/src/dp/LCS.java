0420a0dac4b2097e4eb3eae84ee7d838eafedbf5
Sameer Sarmah <sameerparansarmah@gmail.com>
2020-06-26 23:05:19 +0530

    longest consecutive subsequence
    Change-Id: I3a99ce4e3371a985c1531cef86fede4ede3a2b61
    Signed-off-by: Sameer Sarmah <sameerparansarmah@gmail.com>

--- Algorithm/src/dp/LCS.java
+++ Algorithm/src/dp/LCS.java
@@ -1,5 +1,99 @@
 package dp;
-//longest common subsequence
-public class LCS {
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.javatuples.Pair;
+
+//longest consecutive subsequence
+public class LCS {
+	private static List<Integer> numbers = Arrays.asList(36, 41, 56, 35, 44, 33, 34, 92, 43, 32, 42);
+	private static Map<Integer,Integer> numberMap = new HashMap<>();
+	public static void main(String[] args) {
+		//LicUsingSort(numbers);
+		LicUsingMap( numbers,numberMap);
+	}
+	
+	private static void LicUsingMap(List<Integer> numbers ,Map<Integer,Integer> numberMap) {
+		int maxSequenceLength =0;
+		int maxNumberInSequence = 0;
+		Map<Integer,Integer> numberToSequenceLength = new HashMap<>();
+		for(int number:numbers) {
+			numberMap.put(number, number);
+			numberToSequenceLength.put(number, 1);
+		}
+		for(int index=0;index < numbers.size() ;index++) {
+			if(numberMap.get(numbers.get(index) - 1) == null && numberMap.get(numbers.get(index) + 1) != null ) {
+				//sequence starts with this number
+				Pair<Integer, Integer> sequenceTuple = Pair.with(maxSequenceLength, maxNumberInSequence);
+				sequenceTuple = traverse(numbers.get(index) + 1,numberToSequenceLength,sequenceTuple);
+				System.out.println("for number "+numberMap.get(numbers.get(index))+" ,maxSequenceLength: "+sequenceTuple.getValue0()+" maxNumberInSequence: "+sequenceTuple.getValue1());
+				if(maxSequenceLength < sequenceTuple.getValue0()) {
+					maxSequenceLength = sequenceTuple.getValue0();
+					maxNumberInSequence = sequenceTuple.getValue1();
+				}
+				
+			}
+		}
+		System.out.println("numbers forming the sequence");
+		while(maxSequenceLength>0) {
+			System.out.println(numberMap.get(maxNumberInSequence));
+			--maxNumberInSequence;
+			--maxSequenceLength;
+		}
+		
+	}
+	
+	private static Pair<Integer, Integer> traverse(Integer number ,Map<Integer,Integer> numberToSequenceLength,Pair<Integer, Integer> sequenceTuple) {
+		if(numberMap.get(number) != null && numberMap.get(number - 1) != null) {
+			int maxSequenceLength = sequenceTuple.getValue0();
+		    int maxNumberInSequence = sequenceTuple.getValue1();
+			int sequenceLength = numberToSequenceLength.get(number) + numberToSequenceLength.get(number - 1);
+			numberToSequenceLength.put(number,sequenceLength);
+
+			if(sequenceLength > maxSequenceLength) {
+				maxSequenceLength = sequenceLength;
+				maxNumberInSequence = number;
+			}
+			Pair<Integer, Integer> updatedSequenceTuple = Pair.with(maxSequenceLength, maxNumberInSequence);
+			if(numberMap.get(number + 1) != null) {
+				return traverse(number + 1,numberToSequenceLength,updatedSequenceTuple);
+			}
+			else
+				return updatedSequenceTuple;
+				
+		}
+		return Pair.with(1,number);
+	}
+	
+	private static void LicUsingSort(List<Integer> numbers) {
+		List<Integer> sortedNumbers = numbers.stream().sorted().collect(Collectors.toList());
+		List<Integer> sequence = sortedNumbers.stream().map(n -> 0).collect(Collectors.toList());
+		for(int index=0;index < sequence.size();index++) {
+			if(index == 0) {
+				sequence.set(index,1);
+			}
+			else {
+				if(sortedNumbers.get(index) -1 == sortedNumbers.get(index - 1)) {
+					sequence.set(index,sequence.get(index-1) + 1);//memoized value
+				}
+				else {
+					sequence.set(index,1);
+				}
+			}
+			
+		}
+		int sequenceLength = sequence.stream().max(Integer::compareTo).orElseGet(() -> 0);
+		System.out.println(sequenceLength);
+		int indexOfMaxNumberInSequence = sequence.indexOf(sequenceLength);
+		System.out.println("numbers forming the sequence");
+		for(int index=indexOfMaxNumberInSequence; index > indexOfMaxNumberInSequence - sequenceLength ; index--) {
+			System.out.println(sortedNumbers.get(index));
+		}
+	}
 }
