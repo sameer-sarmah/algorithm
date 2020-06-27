0fcfcaac6ee3efdf98e7b022e0f8a5ffe47526ef
Sameer Sarmah <sameerparansarmah@gmail.com>
2020-06-27 10:50:24 +0530

    refactoring LIS
    Change-Id: I46392d09b7685ddeaacb75ef2fb7ad2c27e88e45
    Signed-off-by: Sameer Sarmah <sameerparansarmah@gmail.com>

--- Algorithm/src/dp/LIS.java
+++ Algorithm/src/dp/LIS.java
@@ -1,65 +1,38 @@
 package dp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//longest increasing subsequence
 class LIS
 {
-   static int max_ref; // stores the LIS
- 
-   /* To make use of recursive calls, this function must return
-   two things:
-   1) Length of LIS ending with element arr[n-1]. We use
-      max_ending_here for this purpose
-   2) Overall maximum as the LIS may end with an element
-      before arr[n-1] max_ref is used this purpose.
-   The value of LIS of full array of size n is stored in
-   *max_ref which is our final result */
-   static int _lis(int arr[], int n)
-   {
-       // base case
-       if (n == 1)
-           return 1;
- 
-       // 'max_ending_here' is length of LIS ending with arr[n-1]
-       int res, max_ending_here = 1;
- 
-        /* Recursively get all LIS ending with arr[0], arr[1] ...
-           arr[n-2]. If   arr[i-1] is smaller than arr[n-1], and
-           max ending with arr[n-1] needs to be updated, then
-           update it */
-        for (int i = 1; i < n; i++)
-        {
-            res = _lis(arr, i);
-            if (arr[i-1] < arr[n-1] && res + 1 > max_ending_here)
-                max_ending_here = res + 1;
-        }
- 
-        // Compare max_ending_here with the overall max. And
-        // update the overall max if needed
-        if (max_ref < max_ending_here)
-           max_ref = max_ending_here;
- 
-        // Return length of LIS ending with arr[n-1]
-        return max_ending_here;
-   }
- 
-    // The wrapper function for _lis()
-    static int lis(int arr[], int n)
-    {
-        // The max variable holds the result
-         max_ref = 1;
- 
-        // The function _lis() stores its result in max
-        _lis( arr, n);
- 
-        // returns max
-        return max_ref;
-    }
- 
-    // driver program to test above functions
     public static void main(String args[])
     {
-        int arr[] = { 10, 22, 9, 13, 18, 30, 41, 60 };
-        int n = arr.length;
-        System.out.println("Length of lis is "
-                           + lis(arr, n) + "n");
+        int numbers[] = { 10, 22, 9, 13, 18, 30, 41, 60 };
+        List<Integer> sequenceLengthByIndex = Arrays.stream(numbers).boxed().map(number -> 1).collect(Collectors.toList());
+        for(int index = 0;index < numbers.length; index++) {
+        	List<Integer> sequenceLengthsForCurrentNumber = new ArrayList<>();
+        	if(index > 0) {
+        		for(int innerIndex = 0;innerIndex < index;innerIndex++) {
+        			if(numbers[index] > numbers[innerIndex]) {
+        				sequenceLengthsForCurrentNumber.add(sequenceLengthByIndex.get(innerIndex));
+        			}
+        		}
+        		if(!sequenceLengthsForCurrentNumber.isEmpty()) {
+        		sequenceLengthByIndex.set(index, Collections.max(sequenceLengthsForCurrentNumber)+1);
+				System.out.println("sequence length for "+numbers[index] +" is "+sequenceLengthByIndex.get(index));
+        		}
+        	}
+        }
+        System.out.println("numbers are");
+        int maxSequenceLength = Collections.max(sequenceLengthByIndex);
+        while(maxSequenceLength> 0) {
+        	int index = sequenceLengthByIndex.indexOf(maxSequenceLength);
+        	System.out.println(numbers[index]);
+        	--maxSequenceLength;
+        }
     }
  }
