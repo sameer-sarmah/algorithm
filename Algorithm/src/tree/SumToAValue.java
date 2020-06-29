8e146041c1cadaaf0cb67f11f000ceee450e3e53
Sameer Sarmah <sameerparansarmah@gmail.com>
2020-06-29 21:24:00 +0530

    refactoring to sum to a value
    Change-Id: I4037ef97ce277e2f298947b1cc0fb74079b3d616
    Signed-off-by: Sameer Sarmah <sameerparansarmah@gmail.com>

--- Algorithm/src/tree/SumToAValue.java
+++ Algorithm/src/tree/SumToAValue.java
@@ -1,40 +1,86 @@
 package tree;
 
-//
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class SumToAValue {
+
 	public static void main(String[] args) {
 		TreeNode<Integer> root = TreeCreator.createBinarySearchTree2();
-		sumToGivenValue(root,12,12,"");
+		Stack<Integer> path = new Stack<Integer>();
+		//List<Integer> pathFound = findSinglePathOfRequiredSum(root,12,path);
+		//System.out.println("found "+ pathFound);
+		List<List<Integer>> pathsFound = findMultiplePathOfRequiredSum(root,12,path);
+		System.out.println(pathsFound);
 	}
-
-	public static void sumToGivenValue(TreeNode<Integer> node, int sum,int requiredSum,String path) {
-		
-		if (node.getValue()==sum) {
-			System.out.println("Found,path to get the sum "+sum+" is "+path+" "+node.getValue());
+	
+	private static List<Integer> findSinglePathOfRequiredSum(TreeNode<Integer> node, int requiredSum,Stack<Integer> path) {
+		List<Integer> pathTillNow = path.stream().collect(Collectors.toList());
+		pathTillNow.add(node.getValue());
+		//System.out.println(pathTillNow);
+		List<Integer> pathFound = doesMatchRequiredSum(requiredSum, pathTillNow);
+		if (!pathFound.isEmpty()) {
+			return pathFound;
 		}
-		
-		if (node.getValue()-requiredSum==0) {
-			System.out.println("Found,path to get the sum "+sum+" is "+path+" "+node.getValue());
-		}
-		
-		
+		path.add(node.getValue());
 		if (node.getLeft() != null) {
-			sumToGivenValue(node.getLeft(),sum, sum - node.getValue()," "+node.getValue());
-		}
-		if(sum - node.getValue()!=requiredSum - node.getValue()) {
-		if (node.getLeft() != null) {
-			sumToGivenValue(node.getLeft(),sum, requiredSum - node.getValue(),path+" "+node.getValue());
-		}
-		}
-		
-		if (node.getRight() != null) {
-			sumToGivenValue(node.getRight(),sum, sum - node.getValue()," "+node.getValue());
+			pathFound = findSinglePathOfRequiredSum(node.getLeft(), requiredSum, path);
+			if (!pathFound.isEmpty()) {
+				return pathFound;
+			}
 		}
 		if (node.getRight() != null) {
-			sumToGivenValue(node.getRight(),sum, requiredSum - node.getValue(),path+" "+node.getValue());
+			pathFound = findSinglePathOfRequiredSum(node.getRight(), requiredSum, path);
+			if (!pathFound.isEmpty()) {
+				return pathFound;
+			}
 		}
-
-
-
+		path.pop();
+		return new ArrayList<>();
+	}
+	
+	private static List<Integer> doesMatchRequiredSum(int requiredSum,List<Integer> path) {
+		List<Integer> pathFound = new ArrayList<>();
+		for(int outerIndex = 0;outerIndex < path.size() ; outerIndex++) {
+			int sum = 0;
+			pathFound.clear();
+			for(int innerIndex = outerIndex;innerIndex < path.size() ; innerIndex++) {
+				sum+=path.get(innerIndex);
+				pathFound.add(path.get(innerIndex));
+			}
+			if(sum == requiredSum) {
+				return pathFound;
+			}
+		}
+		pathFound.clear();
+		return pathFound;
+	}
+	
+	private static List<List<Integer>> findMultiplePathOfRequiredSum(TreeNode<Integer> node,int requiredSum,Stack<Integer> path) {
+		List<Integer> pathTillNow = path.stream().collect(Collectors.toList());
+		pathTillNow.add(node.getValue());
+		//System.out.println(pathTillNow);
+		List<List<Integer>> pathsFound = new ArrayList<>();
+		List<Integer> pathFound = doesMatchRequiredSum(requiredSum, pathTillNow);
+		if (!pathFound.isEmpty()) {
+			pathsFound.add( pathFound);
+		}
+		path.add(node.getValue());
+		if (node.getLeft() != null) {
+			List<List<Integer>> pathsFoundFromLeftSubtree = findMultiplePathOfRequiredSum(node.getLeft(), requiredSum, path);
+			if (!pathsFoundFromLeftSubtree.isEmpty()) {
+				pathsFound.addAll(pathsFoundFromLeftSubtree);
+			}
+		}
+		if (node.getRight() != null) {
+			List<List<Integer>> pathsFoundFromRightSubtree = findMultiplePathOfRequiredSum(node.getRight(), requiredSum, path);
+			if (!pathsFoundFromRightSubtree.isEmpty()) {
+				pathsFound.addAll(pathsFoundFromRightSubtree);
+			}
+		}
+		path.pop();
+		return pathsFound;
 	}
 }
