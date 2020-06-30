8f52823b822bc424e7c31be02f2b665ae4c26ee9
Sameer Sarmah <sameerparansarmah@gmail.com>
2020-06-30 14:06:21 +0530

    adding comments
    Change-Id: I48335d04b3d9f092d67dbbfe481c15c02c7c8f83
    Signed-off-by: Sameer Sarmah <sameerparansarmah@gmail.com>

--- Algorithm/src/tree/FenwickTree.java
+++ Algorithm/src/tree/FenwickTree.java
@@ -6,41 +6,51 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 /*
- *     the lowest 1 bit tells us how many nodes it is responsible for
- 
- e.g 1010 the lowest 1 bit at 2 therefore it is responsible for 9,10
-     0100 the lowest 1 bit at 4 therefore it is responsible for 1,2,3,4
-	 1000 the lowest 1 bit at 8 therefore it is responsible for 1-8
-	 10000 the lowest 1 bit at 16 therefore it is responsible for 1-16
+ *
+ *  length of the fenwick tree is 1 extra than the length of the array it represents
+	we dont store anything at index 0the lowest 1 bit tells us how many nodes it is responsible for
+	
+	responsibleFor in the fenwick tree is the left elements from the fenwickTreeIndex 
+	
+	 0100 the lowest 1 bit at 4 therefore it is responsible for 1,2,3,4 th elements in the array 
+	 1000 the lowest 1 bit at 8 therefore it is responsible for 1-8 th elements in the array 
+	 10000 the lowest 1 bit at 16 therefore it is responsible for 1-16 th elements in the array 
+	 1010 the lowest 1 bit at 2 therefore it is responsible for 9th,10th element of the array located at index 8 and 9
+ 	 therefore the number stored at index 10 in fenwick tree is sum of 9th & 10th element in the array
+
 	 
-	 how to calculate prefix sum
-	 e.g example 1011 =1011 + 1010 + 1000 ,remove the lowest bit each time
+	 remove the right most 1 bit to find the child
+	 add the right most 1 bit to find the parent
+
+	 10 (decimal) has binary rep as 1010 ,to find its parent we find the rightmost bit its 10 we add the decimal value i.e 2  therefore 10+2 =12
+	 10 (decimal) has binary rep as 1010 ,to find its child we find the rightmost bit its 10 we substract the decimal value i.e 2  therefore 10-2 =8
+
 	 
 	 number of bits required to store a number is log n to the base 2 ,e.g to store 16 numbers,0-15, we need 4 bits 2^4 
 	 that is the reason the time complexity is O(log n)
 	 
-	 when we update a node we must also update the nodes which are responsible for the nodes
-	  e.g if we update 1001  ,we have to update 1001 ,1010 ,1100, 10000
-	  we find the parent by adding 1 to the lowest 1 bit ,1001 + 1=1010, 1010+ 0010=1100 ,01100+0100=10000
-	  
-	  to find lowest 1 bit Integer.lowestOnebit 
+	 when we update a node we must also update the nodes which are parents and ancestors for the nodes
+	 we find the parent by adding 1 to the lowest 1 bit ,1001 + 1=1010, 1010+ 0010=1100 ,01100+0100=10000
+	 parent of 1001 is 1010,parent of 1010 is 1100,parent of 1100 is 10000
+	 e.g if we update 1001,1010(parent) ,1100(grandparent), 10000(greatgrandparent)
  * */
 public class FenwickTree {
 	public static final int MAX=16;
 	public static final int MIN=0;
 	public static int[] createFenwickTree(int [] numbers) {
-		int [] tree=new int[numbers.length+1];
+
+		int [] fenwickTree=new int[numbers.length+1];
 		for(int index=0;index<numbers.length;index++) {
 			int fenwickTreeIndex=index+1;
 			int responsibleFor=Integer.lowestOneBit(fenwickTreeIndex);
 			int sum=0;
-			for(int i=fenwickTreeIndex-responsibleFor;i<fenwickTreeIndex;i++) {
-				sum+=numbers[i];
+			for(int indexInArray=fenwickTreeIndex-responsibleFor;indexInArray<fenwickTreeIndex;indexInArray++) {
+				sum+=numbers[indexInArray];
 			}
-			tree[fenwickTreeIndex]=sum;
+			fenwickTree[fenwickTreeIndex]=sum;
 			
 		}
-		return tree;
+		return fenwickTree;
 	}
 	
 
@@ -75,6 +85,7 @@ public class FenwickTree {
 	}
 	
 	//9 will give [9, 8]
+	// find children and all descendants of the index
 	public static List<Integer> getListOfIndicesForPrefixSum(int index){
 		List<Integer> indices=new ArrayList<>();
 		indices.add(index);
@@ -87,6 +98,7 @@ public class FenwickTree {
 		return indices;
 	}
 	// 9 will give [9, 10, 12, 16]
+	// find parent and all ancestors of the index
 	public static List<Integer> getListOfIndicesForUpdate(int index){
 		List<Integer> indices=new ArrayList<>();
 		indices.add(index);
@@ -97,45 +109,17 @@ public class FenwickTree {
 		}
 		return indices;
 	}
-	//10 i.e 1010 will give 1000
-	private static int removeLowestOneBit(int number) {
-		String binary=Integer.toBinaryString(Integer.lowestOneBit(number));
-		int lowestOneBitIndex=binary.length()-binary.indexOf("1");
-	    binary=Integer.toBinaryString(number);
-		int subStringIndex=binary.length()-lowestOneBitIndex;
-		String updatedStr=binary.substring(0, subStringIndex)+"0"+binary.substring(subStringIndex+1);
-		return Integer.parseInt(updatedStr,2); 
-	}
-	
+
 	//10 i.e 1010 will give 2 , lowest one bit is  10
-	private static int getLowestOneBit(int number) {
-		String binary=Integer.toBinaryString(Integer.lowestOneBit(number));
-		int lowestOneBitIndex=binary.length()-binary.indexOf("1");
-        StringBuilder str=new StringBuilder();
-        str.append("1");
-        int counter=lowestOneBitIndex-1;
-        while(counter>0) {
-        	str.append("0");
-        	counter--;
-        }
-		return Integer.parseInt(str.toString(),2); 
+	private static int findParent(int index) {
+		String binary=Integer.toBinaryString(Integer.lowestOneBit(index));
+		return index+Integer.parseInt(binary,2);
 	}
 	
-	private static int findParent(int number) {
-		int numberToAdd=getLowestOneBit(number);
-		return number+numberToAdd;
+	//10 i.e 1010 will give 1000
+	private static int findChild(int index) {
+		String binary=Integer.toBinaryString(Integer.lowestOneBit(index));
+		return index - Integer.parseInt(binary,2);
 	}
-	
-	private static int findChild(int number) {
-		return removeLowestOneBit(number);
-	}
-	//method producing same result as Integer.lowestOneBit
-	@SuppressWarnings("unused")
-	private static int lowestOneBit(int number) {
-		String binaryRep= Integer.toBinaryString(number);
-		String twosComplement= Integer.toBinaryString((~number)+1);
-		twosComplement=twosComplement.substring(twosComplement.length()-binaryRep.length(),twosComplement.length());
-		String str= Integer.toBinaryString(Integer.parseInt(binaryRep, 2) & Integer.parseInt(twosComplement, 2));
-		return str.length()-str.indexOf('1');
-	}
+
 }
