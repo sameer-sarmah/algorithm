4d86cecfd7f73c6845eab0326975e47989d39533
Sameer Sarmah <sameerparansarmah@gmail.com>
2020-07-01 15:36:41 +0530

    refactoring
    Change-Id: I4faa735987003b36229ecd6da5384b4e54d6f9f1
    Signed-off-by: Sameer Sarmah <sameerparansarmah@gmail.com>

--- Algorithm/src/recursion/StrPermutation.java
+++ Algorithm/src/recursion/StrPermutation.java
@@ -11,18 +11,18 @@ public class StrPermutation {
 	public static void main(String[] arg) {
 		List<Character> word = Arrays.asList('a','b','c');
 		List<String> permutations = new ArrayList<>();
-		new StrPermutation().permute(word, 0, word.size(), permutations);
+		new StrPermutation().permute(word, 0, permutations);
 		System.out.println(permutations);
 	}
 
-	private void permute(List<Character> word, int leftIndex, int rightIndex, List<String> permutations) {
-		if (leftIndex >= rightIndex) {
+	private void permute(List<Character> word, int leftIndex, List<String> permutations) {
+		if (leftIndex == word.size()) {
 			permutations.add(word.stream().map(ch -> ch.toString()).reduce((x, y) -> x + y).orElseGet(()->"").toString());
 		} else {
 			List<Character> wordCloned = word.stream().collect(Collectors.toList());
-			for (int swapRightIndex = leftIndex; swapRightIndex < rightIndex; swapRightIndex++) {
+			for (int swapRightIndex = leftIndex; swapRightIndex < word.size(); swapRightIndex++) {
 				Collections.swap(wordCloned, leftIndex, swapRightIndex);// wordCloned="abc" left=0 i=1 after swap bac
-				permute(wordCloned, leftIndex + 1, rightIndex, permutations);
+				permute(wordCloned, leftIndex + 1, permutations);
 			}
 		}
 	}
