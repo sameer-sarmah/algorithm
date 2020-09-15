2966cefcfc1ecb403484081034f20f4072dff4bd
Sameer Sarmah <sameerparansarmah@gmail.com>
2020-09-15 21:01:34 +0530

    comments
    Change-Id: I896d62890630585cfb423105bc585b75cab871c1
    Signed-off-by: Sameer Sarmah <sameerparansarmah@gmail.com>

--- Algorithm/src/matrix/NinetyDegreeShift.java
+++ Algorithm/src/matrix/NinetyDegreeShift.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 Input : {{ 1,  2,  3},
          { 4,  5,  6},
          { 7,  8,  9}}
-Output : {{ 9, 8, 7},
-		  { 6, 5, 4},
-		  { 3, 2, 1}} 
+Output : {{ 3, 6, 9},
+		  { 2, 5, 8},
+		  { 1, 4, 7}} 
 
 column = 2
 rotatedMatrixRow = 0
